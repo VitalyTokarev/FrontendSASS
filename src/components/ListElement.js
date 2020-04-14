11c2a17813536a57d31c340c3e4f709df705d1e4
Vitaly Tokarev <tokarev.exceedteam@gmail.com>
2020-04-14 12:02:00 +0300

    first commit

---
+++ src/components/ListElement.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function ListElement(props) {
+    const {
+        id,
+        valueItem,
+        elemValueClasses,
+        btnEditClasses,
+        editAction,
+        removeButtonDisabled,
+        btnRemoveClasses,
+        removeAction,
+    } = props;
+
+    return(
+        <li>
+            <p className={elemValueClasses}>{valueItem}</p>
+            <button
+                className={btnEditClasses}
+                onClick={() => {editAction(id)}}
+            >Редактировать
+            </button>
+            <button
+                disabled={removeButtonDisabled}
+                className={btnRemoveClasses}
+                onClick={() => {removeAction(id)}}
+            >Удалить
+            </button>
+        </li>
+    );
+}
